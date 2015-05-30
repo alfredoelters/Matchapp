88442bf4eb8a1224752b1e282d6d1d9d1ca48d02
Mathias <cabanomathias@gmail.com>
2015-05-29 22:48:24 -0300

    Improve UI and scrolling in Fixture Details Activity, add dimens, create head to head list item and create list view header.

--- app/src/main/java/android/clase/obligatorio1/fragments/FixtureDetailsFragment.java
+++ app/src/main/java/android/clase/obligatorio1/fragments/FixtureDetailsFragment.java
@@ -3,6 +3,7 @@ package android.clase.obligatorio1.fragments;
 
 import android.clase.obligatorio1.R;
 import android.clase.obligatorio1.entities.Fixture;
+import android.clase.obligatorio1.entities.Match;
 import android.clase.obligatorio1.utils.WebServiceUtils;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -12,17 +13,23 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by alfredo on 17/05/15.
@@ -46,6 +53,8 @@ public class FixtureDetailsFragment extends Fragment {
     private TextView mAwayTeamWins;
     private TextView mDraws;
     private Toolbar mToolbar;
+    private ListView mHeadToHeadListView;
+    private LinearLayout mMatchDetailsLinearLayout;
 
     /**
      * League name obtained from the HomeActivity
@@ -57,7 +66,6 @@ public class FixtureDetailsFragment extends Fragment {
      */
     private Fixture mFixture;
 
-//    private FetchFixtureTask mFetchFixtureTask;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -71,19 +79,24 @@ public class FixtureDetailsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_fixutre_details, container, false);
-        mMatchDayTextView = (TextView) v.findViewById(R.id.matchDayTextView);
-        mMatchDateTextView = (TextView) v.findViewById(R.id.matchDateTextView);
-        mMatchStatusTextView = (TextView) v.findViewById(R.id.matchStatusTextView);
-        mMatchStartTimeTextView = (TextView) v.findViewById(R.id.matchStartTimeTextView);
-        mHomeTeamTextView = (TextView) v.findViewById(R.id.homeTeamTextView);
-        mHomeTeamScoreTextView = (TextView) v.findViewById(R.id.homeTeamScoreTextView);
-        mAwayTeamTextView = (TextView) v.findViewById(R.id.awayTeamTextView);
-        mAwayTeamScoreTextView = (TextView) v.findViewById(R.id.awayTeamScoreTextView);
-        mHomeTeamNameH2H = (TextView) v.findViewById(R.id.home_team_name_h2h);
-        mAwayTeamNameH2H = (TextView) v.findViewById(R.id.away_team_name_h2h);
-        mHomeTeamWins = (TextView) v.findViewById(R.id.home_team_wins);
-        mAwayTeamWins = (TextView) v.findViewById(R.id.away_team_wins);
-        mDraws = (TextView) v.findViewById(R.id.draws);
+        //To add a header to a ListView, you must create a separated layout and inflate it too.
+        View headToHeadListViewHeader = inflater.inflate(R.layout.fixture_fragment_details_header, mHeadToHeadListView, false);
+        mMatchDayTextView = (TextView) headToHeadListViewHeader.findViewById(R.id.matchDayTextView);
+        mMatchDateTextView = (TextView) headToHeadListViewHeader.findViewById(R.id.matchDateTextView);
+        mMatchStatusTextView = (TextView) headToHeadListViewHeader.findViewById(R.id.matchStatusTextView);
+        mMatchStartTimeTextView = (TextView) headToHeadListViewHeader.findViewById(R.id.matchStartTimeTextView);
+        mHomeTeamTextView = (TextView) headToHeadListViewHeader.findViewById(R.id.homeTeamTextView);
+        mHomeTeamScoreTextView = (TextView) headToHeadListViewHeader.findViewById(R.id.homeTeamScoreTextView);
+        mAwayTeamTextView = (TextView) headToHeadListViewHeader.findViewById(R.id.awayTeamTextView);
+        mAwayTeamScoreTextView = (TextView) headToHeadListViewHeader.findViewById(R.id.awayTeamScoreTextView);
+        mHomeTeamNameH2H = (TextView) headToHeadListViewHeader.findViewById(R.id.home_team_name_h2h);
+        mAwayTeamNameH2H = (TextView) headToHeadListViewHeader.findViewById(R.id.away_team_name_h2h);
+        mHomeTeamWins = (TextView) headToHeadListViewHeader.findViewById(R.id.home_team_wins);
+        mAwayTeamWins = (TextView) headToHeadListViewHeader.findViewById(R.id.away_team_wins);
+        mDraws = (TextView) headToHeadListViewHeader.findViewById(R.id.draws);
+        mHeadToHeadListView = (ListView) v.findViewById(R.id.head2headListView);
+        //Add header to ListView, without data and making it no selectable.
+        mHeadToHeadListView.addHeaderView(headToHeadListViewHeader, null, false);
         mToolbar = (Toolbar) v.findViewById(R.id.toolbar);
         mToolbar.setTitle(mLeagueName);
         updateUI();
@@ -101,11 +114,10 @@ public class FixtureDetailsFragment extends Fragment {
         mMatchDateTextView.setText(MATCH_DATE_FORMAT.format(matchDate));
         mMatchStartTimeTextView.setText(getString(R.string.startTime) + MATCH_TIME_FORMAT.format(matchDate));
         mHomeTeamTextView.setText(mFixture.getHomeTeam().getName());
-        //Need to transform the value to a string
         Integer homeTeamScore = mFixture.getGoalsHomeTeam();
+        //If match status isn't finished, the API returns -1 goals for both teams.
         mHomeTeamScoreTextView.setText(homeTeamScore != -1 ? homeTeamScore.toString() : " - ");
         mAwayTeamTextView.setText(mFixture.getAwayTeam().getName());
-        //Need to transform the value to a string
         Integer awayTeamScore = mFixture.getGoalsAwayTeam();
         mAwayTeamScoreTextView.setText(awayTeamScore != -1 ? awayTeamScore.toString() : " - ");
         mHomeTeamNameH2H.setText(mFixture.getHomeTeam().getName());
@@ -113,10 +125,41 @@ public class FixtureDetailsFragment extends Fragment {
         mHomeTeamWins.setText(mFixture.getHead2Head().getHomeTeamWins().toString());
         mAwayTeamWins.setText(mFixture.getHead2Head().getAwayTeamWins().toString());
         mDraws.setText(mFixture.getHead2Head().getDraws().toString());
+        mHeadToHeadListView.setAdapter(new MatchAdapter(mFixture.getHead2Head().getFixtures()));
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+    }
+
+    private class MatchAdapter extends ArrayAdapter<Match> {
+
+        private final DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+        public MatchAdapter(List<Match> matches) {
+            super(getActivity(), 0, matches);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater()
+                        .inflate(R.layout.head_to_head_list_item, null);
+            }
+            Match match = getItem(position);
+            TextView matchDate = (TextView) convertView.findViewById(R.id.h2h_match_date);
+            matchDate.setText(dateFormat.format(match.getDate()));
+            TextView homeTeamName = (TextView) convertView.findViewById(R.id.h2h_home_team);
+            homeTeamName.setText(match.getHomeTeamName());
+            TextView homeTeamScore = (TextView) convertView.findViewById(R.id.h2h_home_team_goals);
+            homeTeamScore.setText(match.getGoalsHomeTeam().toString());
+            TextView awayTeamName = (TextView) convertView.findViewById(R.id.h2h_away_team);
+            awayTeamName.setText(match.getAwayTeamName());
+            TextView awayTeamScore = (TextView) convertView.findViewById(R.id.h2h_away_team_goals);
+            awayTeamScore.setText(match.getGoalsAwayTeam().toString());
+            return convertView;
+        }
+
     }
 }
